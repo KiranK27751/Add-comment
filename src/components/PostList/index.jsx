a5de12ce68a9c747eb776e34698fa88013c704d6
KiranK27751 <kirank27751@gmail.com>
2021-05-29 23:29:32 +0530

    3 files modified

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -5,9 +5,9 @@ import style from './postList.module.scss'
 const PostList = ({comment, onDelete, onDone}) => {
   return(
     <div className={style.postList}>
-      <div className={style.comment}>{comment}</div>
-      <Button buttonText="Delete" onClick={onDelete} customClass='red'/>
-      <Button buttonText="Done" onClick={onDone} customClass='green'/>
+      <div className={style.comment}><p>{comment}</p></div>
+      <div className={style.buttons}><Button buttonText="Delete" onClick={onDelete} customClass='red'/>
+      <Button buttonText="Done" onClick={onDone} customClass='green'/></div>
     </div>
   )
 }
